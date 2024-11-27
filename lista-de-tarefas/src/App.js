994214933e4ce506d72a7d4ac29bec4c6a193978
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-11-27 15:52:24 -0300

    add

---
+++ lista-de-tarefas/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      
+        <p>Teste</p>
+       
+      
+    </div>
+  );
+}
+
+export default App;
